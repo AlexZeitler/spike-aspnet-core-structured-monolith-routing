23b2f2649888c4a8f1ef170c7019b9700a9b5cac
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-07-01 07:31:39 +0200

    chore: allow links in commit footers

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"], rules: {
+    'footer-max-line-length': [0, 'always'] // Make sure there is never a max-line-length by disabling the rule
+  },
+  parserPreset: {
+    parserOpts: {
+      noteKeywords: ['link:'] // Create a custom keyword to distinguish the footer
+    }
+  }
+};
